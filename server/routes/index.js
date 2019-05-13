652cb4a20492faa5bd751097378b2031c84dca46
Carla Mendes <carlarsmendes@gmail.com>
2019-05-13 18:05:27 +0100

    changes to index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,16 +5,15 @@ const Library = require('../models/Library');
 const Book = require('../models/Book');
 
 
-
 //1 - Home Route - Logo, testimonials, Get Started, Description
 router.get('/', (req, res, next) => {
   render('/');
 });
 
 
-//4- Profile-Page: Show Libraries, Books, 
-router.get('/profile-page',  isLoggedIn, (req, res, next) => {
-  Library.find()
+//4- Profile-Page: Show Libraries , Books, 
+router.get('/profile-page/',  isLoggedIn, (req, res, next) => {
+  Library.find({'_members._id':req.user.id})
     .then(librariesFromDb => {
       res.json(librariesFromDb)
         })
@@ -22,7 +21,8 @@ router.get('/profile-page',  isLoggedIn, (req, res, next) => {
       res.json(err);
     })
   
-  Book.find()
+    //only books borrowed by the user
+  Book.find({ "currentOwner.ref._user": req.user.id})
     .then(booksFromDb => {
       res.json(booksFromDb) //change for books which will be shown: only the borrowed books
         })
@@ -31,15 +31,12 @@ router.get('/profile-page',  isLoggedIn, (req, res, next) => {
     })
   });
 
-
-
 //Route Secret --Optional to keep--
-router.get('/secret', isLoggedIn, (req, res, next) => {
-  res.json({
-    secret: 42,
-    user: req.user
-  });
-});
-
+// router.get('/secret', isLoggedIn, (req, res, next) => {
+//   res.json({
+//     secret: 42,
+//     user: req.user
+//   });
+// });
 
 module.exports = router;
