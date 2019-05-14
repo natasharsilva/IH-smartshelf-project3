23a6dc3b329d65e806c3b836676ce1f19c9ef5d1
Natasha R. Silva <natashairsilva@gmail.com>
2019-05-14 20:44:56 +0100

    Book creation fixed

--- server/routes/books.js
+++ server/routes/books.js
@@ -13,13 +13,14 @@ router.post("/", (req, res, next) => {
       Book.create({
         title: response.data.items[0].volumeInfo.title,
         author: response.data.items[0].volumeInfo.authors[0],
-        genre: response.data.items[0].volumeInfo.categories[0],
-        images: response.data.items[0].volumeInfo.imageLinks.thumbnail,
+        genre: response.data.items[0].volumeInfo.categories ? response.data.items[0].volumeInfo.categories[0] : 'Provide a genre',
+        images: response.data.items[0].volumeInfo.imageLinks ? response.data.items[0].volumeInfo.imageLinks[0] : 'Provide an image',
         description: response.data.items[0].volumeInfo.description,
         rating: response.data.items[0].volumeInfo.averageRating,
         pages: response.data.items[0].volumeInfo.pageCount,
         ISBN: response.data.items[0].volumeInfo.industryIdentifiers[1].identifier,
         _createdBy: req.user._id,
+        _library: '5cdb135c23066b50d130b60d'
       }).then(response => {
         res.json({
           message: "Book created!",
