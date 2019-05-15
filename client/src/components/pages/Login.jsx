8829d50385a79e281b79c028262da1e237bdf40b
Carla Mendes <carlarsmendes@gmail.com>
2019-05-15 16:04:42 +0100

    login client

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -14,7 +14,7 @@ export default class Login extends Component {
   
   handleInputChange(event) {
     this.setState({
-      [event.target.username]: event.target.value
+      [event.target.name]: event.target.value
     })
   }
 
