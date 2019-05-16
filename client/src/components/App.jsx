d7aea261e1298a836d1f06e198fb094087521f24
Guilherme Carmona <guilherme.carmona@outlook.com>
2019-05-16 15:31:10 +0100

    navbar added

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Route, Link, NavLink, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Countries from './pages/Countries';
-import AddCountry from './pages/AddCountry';
-import Secret from './pages/Secret';
+import LibraryDetail from './pages/LibraryDetail.js';
+import NavBar from './NavBar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import api from '../api';
@@ -24,24 +24,24 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
+      <NavBar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Project 3</h1>
-          <NavLink to="/" exact>Home</NavLink>
+        
+          {/* <NavLink to="/" exact>Home</NavLink>
           <NavLink to="/countries">Countries</NavLink>
           <NavLink to="/add-country">Add country</NavLink>
           {!api.isLoggedIn() && <NavLink to="/signup">Signup</NavLink>}
           {!api.isLoggedIn() && <NavLink to="/login">Login</NavLink>}
           {api.isLoggedIn() && <Link to="/" onClick={(e) => this.handleLogoutClick(e)}>Logout</Link>}
-          <NavLink to="/secret">Secret</NavLink>
+          <NavLink to="/secret">Secret</NavLink> */}
         </header>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/countries" component={Countries} />
-          <Route path="/add-country" component={AddCountry} />
+          <Route path="/librares/:libraryId" component={LibraryDetail} />
           <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
-          <Route path="/secret" component={Secret} />
           <Route render={() => <h2>404</h2>} />
         </Switch>
       </div>
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Collapse,Navbar,NavbarToggler,NavbarBrand,Nav,NavItem,NavLink,UncontrolledDropdown,DropdownToggle,DropdownMenu,
+  DropdownItem, } from 'reactstrap';
+import api from "../api";
+import {NavLink as Nlink} from 'react-router-dom';
+
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          {/* <NavbarBrand href="/">Project3</NavbarBrand> */}
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink tag={Nlink} to="/home">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Nlink} to="/profile">Profile</NavLink>
+              </NavItem>
+              <NavItem>
+          {!api.isLoggedIn() && <NavLink tag={Nlink} to="/login">Login</NavLink>}
+              </NavItem>
+              <NavItem>
+              {!api.isLoggedIn() && <NavLink tag={Nlink} to="/signup">Signup</NavLink>}
+              </NavItem>
+              <NavItem>
+          {api.isLoggedIn() && <NavLink tag={Nlink} to="/" onClick={(e) => this.handleLogoutClick(e)}>Logout</NavLink>}
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+
+    )}
+  }
