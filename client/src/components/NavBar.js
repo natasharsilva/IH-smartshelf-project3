73e8f02cfc87bbb02cffaddb4a0592f49841dc1e
Carla Mendes <carlarsmendes@gmail.com>
2019-05-22 17:15:10 +0100

    navbar corrected

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -55,25 +55,6 @@ export default class Example extends React.Component {
                 )}
               </NavItem>
               <NavItem>
-
-                <NavLink tag={Nlink} to="/add-library">Add Library</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Nlink} to="/libraries/:libraryId">Library Detail</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Nlink} to="/:libraryId/books">Library Books</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Nlink} to="/:libraryId/add-book">Add Book</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Nlink} to="/books/:bookId">Book Detail</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Nlink} to="/find-libraries">Find Libraries</NavLink>
-              </NavItem>
-              <NavItem>
           {!api.isLoggedIn() && <NavLink tag={Nlink} to="/login">Login</NavLink>}
 
               </NavItem>
