de743a7b478b2dd233985c641c2b5827344db0ab
Carla Mendes <carlarsmendes@gmail.com>
2019-05-24 00:04:37 +0100

    styling book details

--- client/src/components/pages/BookDetail.jsx
+++ client/src/components/pages/BookDetail.jsx
@@ -1,6 +1,8 @@
   
 import React, { Component } from "react";
 import api from "../../api";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBook,faComment } from '@fortawesome/free-solid-svg-icons'
 import {
   Alert,
   Card,
@@ -11,7 +13,8 @@ import {
   CardSubtitle,
   Button,
   Row,
-  Col
+  Col,
+  Container
 } from "reactstrap";
 import Rating from '../Rating'
 import AddReview from "./AddReview"
@@ -93,30 +96,33 @@ export default class BookDetail extends Component {
 
   render() {
     return (
-      <div>
+      <div className="BookDetail">
         {!this.state.book && <div>Loading...</div>}
         {this.state.book && (
           <div className="bookCard">
             <Card>
+            <CardBody>
+            <Container>
               <Row>
-                <Col>
+                <Col s='3'>
                   <CardImg
-                    top
-                    width="100%"
+                    body
                     src={this.state.book.picture}
                     alt="Card image cap"
+                    style={{width:'100px',objectFit:'cover'}}
                   />
                 </Col>
-                <CardBody>
-                  <CardTitle tag="h3">
+                <Col s='9'>
+                  <CardTitle className="text-left" tag="h4">
                     <strong>{this.state.book.title}</strong>
                   </CardTitle>
-                  <CardSubtitle tag="h4">{this.state.book.author}</CardSubtitle>
-                  <CardText>
-                    {this.state.book.description}
+                  <CardSubtitle  className="text-left" tag="h6"><i>by {this.state.book.author}</i></CardSubtitle>
+                  <CardText className="text-left">
+                    <strong>Genre: </strong>
+                    {this.state.book.genre}
                     <br />
                     <strong>Rating: </strong>
-                    {this.state.book.rating}
+                    {this.state.book.rating}/5
                     <br />
                     <strong>Pages: </strong>
                     {this.state.book.pages}
@@ -124,20 +130,28 @@ export default class BookDetail extends Component {
                     <strong>ISBN: </strong>
                     {this.state.book.isbn}
                     <br />
+                    
                   </CardText>
+                  </Col>
+                  </Row>
+                  </Container>
+                  
+                  {this.state.book.description}
+                  <br />
                   {this.state.book.status === "Available" && (
                     <Button
                       onClick={e => this.borrowBook(e)}
                       outline
-                      color="info"
+                      className="round-btn"
                     >
-                      Borrow
+                      <FontAwesomeIcon icon={faBook} size="1x" className="icon"/>{' '}borrow this book
                     </Button>
                   )}
                   {this.state.book.status === "Unavailable" && 
                         <div><br /><Alert color="warning" >This book is not available at the moment - it has been borrowed.</Alert></div> }
                   <br />
-                  <Button onClick={this.renderReviewForm}>Add a review</Button>
+                  <Button onClick={this.renderReviewForm}>
+                  <FontAwesomeIcon icon={faComment} size="1x" className="icon"/>{' '}add a review</Button>
                   {this.state.showReviewForm && 
                   <AddReview onToggle={this.renderReviewForm} theInfo={this.state.response} />}<br />
                   <Button href={`/report-problem/${this.state.book._library}`} outline color="info" size="sm"
@@ -145,14 +159,13 @@ export default class BookDetail extends Component {
                     Report a problem
                   </Button>
                   <br />
-                  <Button color="primary" href={`/profile`}>
+                  <Button href={`/profile`}>
                     Go Back
                   </Button><br />
                   {this.state.message && (
                     <div className="info">{this.state.message}</div>
                   )}
                 </CardBody>
-              </Row>
             </Card>
             <Card className="reviewContainer">
               <CardBody>
