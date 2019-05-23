ffacc1523a648003c92ed9962565147b82294ffe
Carla Mendes <carlarsmendes@gmail.com>
2019-05-23 19:30:59 +0100

    changes to home

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -8,17 +8,17 @@ import './../../index.scss';
 
 const items = [
   {
-    // src: '../../../public/images/logo.svg',
-    altText: `"What an amazing app! Now I can share my books with everyone from my company"`,
-    //  caption: 'Slide 1'
+     src: '../../../images/carousel-joaquim.jpg',
+     altText: `"What an amazing app! Now I can share my books with everyone from my company"`,
+    // caption: 'Slide 1'
   },
   {
-    // src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+    src: '../../../images/carousel-maxence.jpg',
     altText: 'Amazing!',
     // caption: 'Slide 2'
   },
   {
-    // src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+    src: '../../../images/carousel-mostafa.jpg',
     altText: '"Love it!"',
     // caption: 'Slide 3'
   }
@@ -84,7 +84,10 @@ export default class Home extends Component {
     return (
       <div className="Home">
         <div className="home-header">
-          <h2>Smart Shelf</h2>
+          <div className="SmartShelfLogo">
+            <img src="../../../images/Symbol_Negative.svg" alt="logoSymbol" id="symbol"></img>
+            <img src="../../../images/Logotype_Negative.svg" alt="logotype" id="logo"></img>
+          </div>
           <Button href="/signup" className="button-home" border-radius="30px" block>Get Started</Button>
         </div>
         <div className="home-features">
@@ -113,7 +116,7 @@ export default class Home extends Component {
           </Link>
         </div>
         <div className="testimonials features">
-          <h2>Testimonials</h2>
+          <h2>Our Happy Smart Shelfers</h2>
           <Carousel
               activeIndex={activeIndex}
               next={this.next}
